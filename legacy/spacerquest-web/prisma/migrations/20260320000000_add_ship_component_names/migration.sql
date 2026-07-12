afc0c77127e5b270a18693a24ea178a1b31ff4c9
VS7 <214502315+ArchitectVS7@users.noreply.github.com>
2026-07-12 10:06:00 -0500

    Remove Legacy and Foundation material, Audit initial coding task

--- legacy/spacerquest-web/prisma/migrations/20260320000000_add_ship_component_names/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Ship" ADD COLUMN "hullName" TEXT,
-ADD COLUMN "driveName" TEXT,
-ADD COLUMN "cabinName" TEXT,
-ADD COLUMN "lifeSupportName" TEXT,
-ADD COLUMN "weaponName" TEXT,
-ADD COLUMN "navigationName" TEXT,
-ADD COLUMN "roboticsName" TEXT,
-ADD COLUMN "shieldName" TEXT;
